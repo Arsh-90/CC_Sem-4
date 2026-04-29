28da0bdeb05babbce864f1a84f0b6a4cc3e2de52
Arsh-90 <24bcs12040@cuchd.in>
2026-04-29 21:17:01 +0530

    Add files via upload

---
+++ CC_final_practical/leetcode-42.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+    int left = 1;
+    int right = height.size() - 2;
+
+    int lMax = height[left - 1];
+    int rMax = height[right + 1];
+
+    int res = 0;
+    while (left <= right) {
+      
+        if (rMax <= lMax) {
+          
+            res += max(0, rMax -height[right]);
+
+            
+            rMax = max(rMax, height[right]);
+
+            
+            right -= 1;
+        } else { 
+           
+            res += max(0, lMax - height[left]);
+
+            lMax = max(lMax, height[left]);
+            left += 1;
+        }
+    }
+    return res;
+}
+};
+
