48a0033b1f62c53938f4cbae532b97d954b9ca9c
Arsh-90 <24bcs12040@cuchd.in>
2026-08-06 22:26:40 +0530

    Time: 14 ms (92.05%), Space: 51.9 MB (6.59%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int res=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int n =g.length;
+        int m=s.length;
+        while(i<n &&j<m){
+            if(s[j]>=g[i]){
+                res++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }return res;
+
+        
+    }
+}
