8aa40c29ef490ff427570e1f01e96bc902fd9738
Arsh-90 <24bcs12040@cuchd.in>
2026-08-17 19:21:43 +0530

    Time: 19 ms (91.78%), Space: 54.2 MB (20.82%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,36 @@
+class Solution {
+    int[][] dp;
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        dp = new int[n + 1][m + 1];
+
+        // Base cases
+        for (int i = 0; i <= n; i++) {
+            dp[i][m] = 0;
+        }
+
+        for (int j = 0; j <= m; j++) {
+            dp[n][j] = 0;
+        }
+
+        // Fill DP table from bottom-right
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(
+                        dp[i + 1][j],
+                        dp[i][j + 1]
+                    );
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
